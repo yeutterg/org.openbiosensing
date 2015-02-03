c478b95b75d93e48ebf10327b73184eac99f57cf
Greg Yeutter <yeutterg@gmail.com>
2015-02-03 11:32:10 -0500

    intellij refactor freeze

--- org.openbiosensing/src/org/openbiosensing/Fitbit.java
+++ org.openbiosensing/src/org/openbiosensing/Fitbit.java
@@ -1,30 +1,45 @@
 package org.openbiosensing;
 
-import org.scribe.builder.api.DefaultApi10a;
-import org.scribe.model.Token;
+import org.scribe.builder.*;
+import org.scribe.builder.api.*;
+import org.scribe.model.*;
+import org.scribe.oauth.*;
+
+import java.util.Scanner;
 
 /**
  * Created by Greg on 1/25/2015.
  */
+public class Fitbit {
+    private String apiKey = "1cc9c62eb82f40a5910cced078f3a0a5";
+    private String apiSecret = "693ec81e319641208ee7569b50cb7665";
+    private String PROTECTED_RESOURCE_URL = "http://api.fitbit.com/1/user/-/profile.json";
 
-public class Fitbit extends DefaultApi10a{
-    public static final String BASE_URL = "http://api.fitbit.com/";
-    private static final String REQUEST_TOKEN_RESOURCE = "api.fitbit.com/oauth/request_token";
-    private static final String AUTHORIZE_URL = "http://api.fitbit.com/oauth/authorize?oauth_token=%s";
-    private static final String ACCESS_TOKEN_RESOURCE = "api.fitbit.com/oauth/access_token";
+    // Create OAuthService Object
+    OAuthService service = new ServiceBuilder()
+            .provider(FitbitOAuth.class)
+            .apiKey(apiKey)
+            .apiSecret(apiSecret)
+            .build();
 
-    @Override
-    public String getRequestTokenEndpoint() {
-        return "https://" + ACCESS_TOKEN_RESOURCE;
-    }
+    // Get Request Token
+    Token requestToken = service.getRequestToken();
 
-    @Override
-    public String getAccessTokenEndpoint() {
-        return "https://" + REQUEST_TOKEN_RESOURCE;
-    }
+    // Validate Request Token
+    String authUrl = service.getAuthorizationUrl(requestToken);
 
-    @Override
-    public String getAuthorizationUrl(Token requestToken) {
-        return String.format(AUTHORIZE_URL, requestToken.getToken());
-    }
+    // Get Access Token
+    System.out.println("Please enter the verifier: ")
+    Scanner sc = new Scanner(System.in);
+    String input = sc.nextLine();
+    Verifier v = new Verifier(input);
+    Token accessToken = service.getAccessToken(requestToken, v);
+
+    // Sign Request
+    OAuthRequest request = new OAuthRequest(Verb.GET, PROTECTED_RESOURCE_URL);
+    service.signRequest();
+    Response response = request.send();
+    System.out.println(response.getBody());
+
+
 }
